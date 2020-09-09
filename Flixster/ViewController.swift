23f5fffd84333437ea5085cefeb065a385dd8e7d
Tiffany Medina <tdm16g@my.fsu.edu>
2020-09-09 16:21:23 -0400

    Initial Commit

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by Tiffany Medina on 9/9/20.
+//  Copyright © 2020 tiffanymedina23. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
